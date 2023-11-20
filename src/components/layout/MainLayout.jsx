8a29d017286256c2654664e1e698cf644c82d75e
Lê Văn Trung <49792818+letrung02082000@users.noreply.github.com>
2023-11-20 15:00:29 +0700

    update layout

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,12 +1,39 @@
 import React, { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, Outlet } from 'react-router-dom';
+import { PATH } from '../../constants/path';
 
 function MainLayout() {
-  return <>
-    <Suspense fallback={<div>Loading...</div>}>
-        <Outlet/>
-    </Suspense>
-  </>;
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar expand='lg' className='bg-body-tertiary'>
+          <Container>
+            <Navbar.Brand href='/'>WNC</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='me-auto w-100'>
+                <div className='d-flex justify-content-between w-100'>
+                  <div>
+                    <Nav.Link href='/'>Trang chủ</Nav.Link>
+                  </div>
+                  <div className='d-flex'>
+                    <Nav.Link>
+                      <Link to={PATH.AUTH.SIGNUP}>Đăng ký</Link>
+                    </Nav.Link>{' '}
+                    <Nav.Link>
+                      <Link to={PATH.AUTH.SIGNIN}>Đăng nhập</Link>
+                    </Nav.Link>
+                  </div>
+                </div>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Outlet />
+      </Suspense>
+    </>
+  );
 }
 
 export default MainLayout;
--- src/features/home/index.jsx
+++ src/features/home/index.jsx
@@ -8,29 +8,7 @@ import { PATH } from '../../constants/path';
 
 function HomePage() {
   return (
-    <Navbar expand='lg' className='bg-body-tertiary'>
-      <Container>
-        <Navbar.Brand href='#'>WNC</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto w-100'>
-            <div className='d-flex justify-content-between w-100'>
-              <div>
-                <Nav.Link href='#'>Trang chủ</Nav.Link>
-              </div>
-              <div className='d-flex'>
-                <Nav.Link>
-                  <Link to={PATH.AUTH.SIGNUP}>Đăng ký</Link>
-                </Nav.Link>{' '}
-                <Nav.Link>
-                  <Link to={PATH.AUTH.SIGNUP}>Đăng nhập</Link>
-                </Nav.Link>
-              </div>
-            </div>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Container>Home Page</Container>
   );
 }
 
