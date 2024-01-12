7ebc0bc63e744b3b5f4d41bec4b4204285e29e18
Lê Văn Trung <49792818+letrung02082000@users.noreply.github.com>
2024-01-13 06:13:50 +0700

    update api url

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,3 +1,3 @@
-// export const API_URL = import.meta.env.VITE_API_URL || 'https://web-2324-project.vercel.app/'
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+export const API_URL = import.meta.env.VITE_API_URL || 'https://web-2324-project.vercel.app/'
+// export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 export const GOOGLE_LOGIN_URL = `${API_URL}/auth/google`
